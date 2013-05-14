3c255865a122ba97267e8167b91387b0b16ade90
Guy Vider <guy@guyvider.com>
2013-05-14 11:39:58 -0700

    added timeout support; JSHinted code; fixed README headers

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 MyIP - geolocate by IP
-----------------------
+======================
 
 This little app shows your external IP and then uses uses the [IPInfoDB](http://ipinfodb.com/) API to provide geolocation data, based on that IP.
 Currently, results are offered in JSON format only.
 
 Motivation
-==========
+----------
 I'm on the road quite a lot, and many times I need to know my external IP. Oftentimes, it's good to know where web sites think I'm calling from.
 I wrote a version of this in PHP, that still works great, but I wanted to try a simple Node.js app. The idea was to try writing this as simple as possible, with no external modules or dependencies. After finishing the server side, I wrote a simple Chrome extension (see below) that consumes the JSON and shows it in a browser.
 
 Usage
-=====
+-----
  1. Clone the repo (if you're here, you know how to do that :))
  2. Go to [IPInfoDB API](http://ipinfodb.com/register.php) and register for the free API key
  3. Paste the API key into the [config.json](https://github.com/TravelingTechGuy/myip/blob/github/config.json) file
@@ -20,7 +20,7 @@ Usage
 You can test a running version at [http://MyIP.TravelingTechGuy.com](http://myip.travelingtechguy.com/) (hosted on Heroku).
 
 Chrome extension
-================
+----------------
 You can check out the Chrome extension that uses the service to show the info in the Chrome browser.
  - Find the repository here [https://github.com/TravelingTechGuy/myipextension](https://github.com/TravelingTechGuy/myipextension) 
  - Download from the [Chrome app store](https://chrome.google.com/webstore/detail/my-ip/lejbibljgiojigkpkhmdgdhmiaddgidd?hl=en-US&gl=US).
--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-var http = require('http');
-var config = require('./config');
-var port = process.env.PORT || 8080;
+'use strict';
+
+var http = require('http')
+,	config = require('./config')
+,	port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
 	//avoid double call if it's a browser call for favicon
-	if(req.url.indexOf('favicon') != -1)
+	if(req.url.indexOf('favicon') !== -1)
 		return;
-	
-	var ip = req.headers["remote-addr"] || req.headers["x-forwarded-for"];
+
+	var ip = req.headers['remote-addr'] || req.headers['x-forwarded-for'];
 	if(ip) {
-		getIPInfo(ip, function(data) {
-			printResult(res, data);
+		getIPInfo(ip, function(data, error) {
+			printResult(res, data, error);
 		});
 	}
 	else {
 		console.log(JSON.stringify(req.headers));
-		printResult(res, JSON.stringify({'error': 'Could not find the originating IP address'}));
+		printResult(res, null, 'Could not find the originating IP address');
 	}
-  	
 }).listen(port, function() {
 	console.log('Listening on port ' + port);
 });
 
-var printResult = function(res, data) {
+var printResult = function(res, data, error) {
+	if(error) {
+		data = JSON.stringify({'error': error});
+	}
 	res.writeHead(200, {'Content-Type': 'application/json'});
   	res.end(data);
 };
 
 var getIPInfo = function(ip, callback) {
-	var url = config.url + "?key=" + config.apiKey + "&ip=" + ip + "&output=json&timezone=true";
+	var url = config.url + '?key=' + config.apiKey + '&ip=' + ip + '&output=json&timezone=true';
 	var req = http.request(url, function(response) {
   		response.on('data', function (data) {
-    		callback(data);
-  		});
+			callback(data, false);
+		});
+	});
+
+	//abort request if it takes over 10 seconds
+	req.setTimeout(10000, function() {
+		req.abort();
+		console.log('Call to ipinfodb timed out');
+		callback(null, 'Timeout occured');
 	});
 
 	req.on('error', function(e) {
-  		console.log('problem with request: ' + e.message);
+		console.log('problem with request: ' + e.message);
+		callback(null, 'Error occured while trying to identify IP address');
 	});
 
 	req.end();
